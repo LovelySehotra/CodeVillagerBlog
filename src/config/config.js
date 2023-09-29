d1981d06ec95d91a8f069f1c2ca0f017a99fe3b8
Lovely Sehotra <lsehotra12@gmail.com>
2023-09-29 22:27:02 +0530

    update

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-const config ={
-    appwwriteUrl : String(import.meta.env.VITE_APPWRITE_URL),
-    appwwriteProjectId : String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-    appwwriteDatabaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    appwwriteCollectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-    appwwriteBucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-}
-export default config
