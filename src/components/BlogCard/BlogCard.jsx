de6f2257a79eaa373cf127694ababf74d9a6d14f
Lovely Sehotra <lsehotra12@gmail.com>
2024-02-14 23:34:34 +0530

    fix css for blogcard

--- src/components/BlogCard/BlogCard.jsx
+++ src/components/BlogCard/BlogCard.jsx
@@ -10,13 +10,13 @@ const BlogCard =()=>{
                     <img src={featured} alt="" />
                     </div>
                     <div className="blog-content">
-                    <span>
+                    <span className="content-info">
                         <p>Mar 23, 2023</p>
                         <p>
                             2 min</p>
                     </span>
-                    <h3>Back to Fiction: What I'm Reading This Summer</h3>
-                    <p>
+                    <h3 className="content-title">Back to Fiction: What I'm Reading This Summer</h3>
+                    <p className="content-paragraph">
                         Create a blog post subtitle that summarizes your post in a few short, punchy sentences and entices your audience to continue reading....
                     </p>
                     </div>
